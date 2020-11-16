4dad52cb91b8f34980e9d812acf4987029cfbbdd
GranPecador <lennuz17@gmail.com>
2020-11-16 17:26:44 +0300

    fix navigation in waiter page

--- app/src/main/java/com/example/restic/WaiterComponent.kt
+++ app/src/main/java/com/example/restic/WaiterComponent.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
@@ -20,77 +17,42 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigate
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 
 class WaiterComponent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ConstraintLayout (modifier = Modifier.fillMaxHeight().fillMaxWidth(),
-                constraintSet = ConstraintSet {
-                    val nameRestic = createRefFor("nameRestic")
-                    val text2 = createRefFor("fragment")
-                    val bottomNav = createRefFor("bottomNav")
-
-                    constrain(nameRestic) {
-                        start.linkTo(parent.start, margin = 0.dp)
-                        end.linkTo(parent.end, margin = 10.dp)
-                        top.linkTo(parent.top, margin = 10.dp)
-                    }
-                    constrain(text2) {
-                        //centerTo(parent)
-                        top.linkTo(nameRestic.bottom, 5.dp)
-                        start.linkTo(parent.start, margin = 10.dp)
-                        end.linkTo(parent.end, margin = 10.dp)
-                        bottom.linkTo(bottomNav.top, margin = 0.dp)
-
-                    }
-                    //val barrier = createBottomBarrier(text1, text2)
-                    constrain(bottomNav) {
-                        bottom.linkTo(parent.bottom, margin = 0.dp)
-                        start.linkTo(parent.start, margin = 0.dp)
-                        end.linkTo(parent.end, margin = 0.dp)
-                        //centerHorizontallyTo(parent)
-                        //width = Dimension.preferredWrapContent.atMost(40.dp)
-                    }
-                }
+            val navController = rememberNavController()
+            Scaffold(
+                topBar = { NameRestaurant() },
+                bottomBar = { WaiterBottomNavigation(navController = navController) }
             ) {
-                NameRestaurant()
-                val navController = rememberNavController()
-                    NavHost(navController, startDestination = "booking") {
-                        composable("booking") { Booking() }
-                        composable("tables") { Tables() }
-                        composable("settings") { Settings()}
-                    }
-                WaiterBottomNavigation(navController)
+                NavHost(navController = navController, startDestination = "booking") {
+                    composable("booking") { Booking(navController) }
+                    composable("tables") { Tables() }
+                    composable("settings") { Settings() }
+                }
+
             }
         }
     }
 }
 
 @Composable
-fun Booking() {
-    Box(modifier = Modifier.layoutId("fragment")){
+fun Booking(navController: NavController ) {
         Text("book")
 
-    }
 }
 
 @Composable
 fun Tables() {
-    Box(modifier = Modifier.layoutId("fragment")) {
         Text("table")
-    }
 }
 
 @Composable
 fun Settings() {
-    Box(modifier = Modifier.layoutId("fragment")) {
         Text("Settings")
-    }
 }
 
 @Composable
@@ -98,29 +60,32 @@ fun WaiterBottomNavigation(navController: NavController) {
     var selectedItem by remember { mutableStateOf(0) }
     val items = listOf("Бронь", "Столик", "Настройки")
     val fragmentsItem = listOf("booking", "tables", "settings")
-    BottomNavigation(modifier =Modifier.layoutId("bottomNav"),
-    ) {
+    BottomNavigation{
         items.forEachIndexed { index, item ->
             BottomNavigationItem(
                 icon = { Icon(Icons.Filled.Favorite) },
                 label = { Text(item) },
                 selected = selectedItem == index,
                 onClick = {
-                    selectedItem = index
-                    navController.navigate(route = fragmentsItem[index])
+                    //navController.popBackStack(navController.graph.startDestination, false)
+                    if (selectedItem != index) {
+                        selectedItem = index
+                        navController.navigate(route = fragmentsItem[index])
+                    }
                 },
                 alwaysShowLabels = false,
             )
         }
     }
 }
+
 @Composable
 fun NameRestaurant() {
     Text(
         text = "Ресторан №1",
         fontSize = 18.sp,
         textDecoration = TextDecoration.Underline,
-        modifier = Modifier.fillMaxWidth().layoutId("nameRestic"),
+        modifier = Modifier.fillMaxWidth(),
         textAlign = TextAlign.Center,
     )
 }
